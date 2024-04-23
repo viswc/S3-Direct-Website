cb77b71992502ee6aaffdb8519334bce56b12097
Viswam PC <75151315+viswc@users.noreply.github.com>
2024-04-23 16:55:57 +0530

    AuthModels

--- S3DirectWebsite/Authenticate/models.py
+++ S3DirectWebsite/Authenticate/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+import uuid
 
-# Create your models here.
+class Account(models.Model):
+    dateCreated = models.DateTimeField(auto_now_add = True)
+    dateModified = models.DateTimeField(auto_now = True)
+    primaryKey = models.UUIDField(default=uuid.uuid4, editable=True, unique=True)
+
+    Username = models.OneToOneField(User, on_delete=models.CASCADE)
+    isActive = models.BooleanField(default=False)
+    email = models.CharField(max_length=100, default="")
+    firstName = models.CharField(max_length=100, default="")
+    lastName = models.CharField(max_length=100, default="")
